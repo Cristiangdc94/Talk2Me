b4cbf58a9df1c24a25495a8cdba091f2ab99f866
Cristian ye <cristiangc69coontrol@gmail.com>
2025-10-14 11:04:35 +0000

    Por alguna razon se siguen contrayendo

--- src/app/coworkers/page.tsx
+++ src/app/coworkers/page.tsx
@@ -5,10 +5,8 @@ import { users } from "@/lib/mock-data";
 export default function CoworkersPage() {
   const coworkers = users.filter((user) => user.relationship === 'coworker');
   return (
-    <div className="h-full flex flex-col">
-      <div className="p-4 sm:p-6 flex-1">
-        <UserList title="Compañeros de Trabajo" users={coworkers} />
-      </div>
+    <div className="h-full flex flex-col p-4 sm:p-6">
+      <UserList title="Compañeros de Trabajo" users={coworkers} />
     </div>
   );
 }
--- src/app/friends/page.tsx
+++ src/app/friends/page.tsx
@@ -5,10 +5,8 @@ import { users } from "@/lib/mock-data";
 export default function FriendsPage() {
   const friends = users.filter((user) => user.relationship === 'friend');
   return (
-    <div className="h-full flex flex-col">
-      <div className="p-4 sm:p-6 flex-1">
-        <UserList title="Amigos" users={friends} />
-      </div>
+    <div className="h-full flex flex-col p-4 sm:p-6">
+      <UserList title="Amigos" users={friends} />
     </div>
   );
 }
