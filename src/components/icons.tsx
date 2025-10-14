4815ad174e90833d0571f90363838a64ff281f17
Cristian ye <cristiangc69coontrol@gmail.com>
2025-10-14 18:26:15 +0000

    No se ha cambiado, sigue siendo gris, eliminalo y rehazlo de nuevo

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -8,7 +8,6 @@ export function AppLogo(props: SVGProps<SVGSVGElement>) {
       height="1em"
       viewBox="0 0 24 24"
       fill="currentColor"
-      stroke="currentColor"
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
