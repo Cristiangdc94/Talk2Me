cf91d15ef7b1ec0a7b67b44b31c8911dcadef7db
Cristian ye <cristiangc69coontrol@gmail.com>
2025-10-14 09:42:21 +0000

    Cambia la fuente de "Canales" y "Mensajes directos" por algo mas informa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
           rel="stylesheet"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&display=swap"
           rel="stylesheet"
         />
         <link 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['"PT Sans"', 'sans-serif'],
-        headline: ['"Playfair Display"', 'serif'],
+        headline: ['"Dancing Script"', 'cursive'],
         logo: ['"Permanent Marker"', 'cursive'],
         code: ['monospace'],
       },
