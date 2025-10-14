48429351d2bb09003aa21efdcdce1a28c01d0d67
Cristian ye <cristiangc69coontrol@gmail.com>
2025-10-14 20:43:07 +0000

    Añade espacio al final del menú para que no pueda solaparse el SidebarFo

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -411,7 +411,7 @@ const SidebarContent = React.forwardRef<
       ref={ref}
       data-sidebar="content"
       className={cn(
-        "flex min-h-0 flex-1 flex-col gap-2 overflow-auto group-data-[collapsible=icon]:overflow-hidden",
+        "flex min-h-0 flex-1 flex-col gap-2 overflow-auto group-data-[collapsible=icon]:overflow-hidden pb-4",
         className
       )}
       {...props}
@@ -758,5 +758,7 @@ export {
   SidebarTrigger,
   useSidebar,
 }
+
+    
 
     
