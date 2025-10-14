fc57ae9adb378d0e80793bf0a1cf00f00fdf98bb
Cristian ye <cristiangc69coontrol@gmail.com>
2025-10-14 16:52:51 +0000

    Try fixing this error: `Console Error: In HTML, <button> cannot be a des

--- src/components/chat/sidebar-nav.tsx
+++ src/components/chat/sidebar-nav.tsx
@@ -175,7 +175,7 @@ export function SidebarNav() {
         <SidebarGroupLabel className="font-headline text-xl">
           Mensajes Directos
         </SidebarGroupLabel>
-        <SidebarGroupAction>
+        <SidebarGroupAction asChild>
           <NewMessagePopover />
         </SidebarGroupAction>
         <SidebarMenu>
