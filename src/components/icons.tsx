d8fa2d9b79bad81f44c28950ebe106ce7c73bdcc
Cristian ye <cristiangc69coontrol@gmail.com>
2025-10-14 18:26:58 +0000

    Este bocadillo sigue siendo gris, eliminalo y crealo de nuevo en color b

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -8,9 +8,6 @@ export function AppLogo(props: SVGProps<SVGSVGElement>) {
       height="1em"
       viewBox="0 0 24 24"
       fill="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
       {...props}
     >
       <path d="M17.5 3h-11A4.5 4.5 0 0 0 2 7.5v6A4.5 4.5 0 0 0 6.5 18H7l4 4v-4h6.5A4.5 4.5 0 0 0 22 13.5v-6A4.5 4.5 0 0 0 17.5 3Z" />
